3083f842cb7bd360cf64ffb53a308fbfa97a1c7d
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2020-10-02 11:34:05 +0200

    Update pin_access.dart

--- flutter Apps/flutter_app_dental_uvc/lib/pages/pin_access.dart
+++ flutter Apps/flutter_app_dental_uvc/lib/pages/pin_access.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutterappdentaluvc/services/CSVfileClass.dart';
 import 'package:flutterappdentaluvc/services/bleDeviceClass.dart';
 import 'package:flutterappdentaluvc/services/uvcToast.dart';
 import 'package:path_provider/path_provider.dart';
