285c11b698d07d099bde9038c58ffb63d195ddf4
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2021-01-20 21:13:23 +0100

    update safe uvc qrcode

--- flutter Apps/flutter_safe_uvc_qrcode_app/lib/pages/choose_qrcode.dart
+++ flutter Apps/flutter_safe_uvc_qrcode_app/lib/pages/choose_qrcode.dart
@@ -44,20 +44,17 @@ class _ChooseQrCodeState extends State<ChooseQrCode> {
                           buttonDescription2: '- les informations du QR code Data\n'
                               '+\n'
                               '- Un code PIN (optionnel)\n'),
-                      qrCodeGenerator(
+                      qrCodeGeneratorSecond(
                           context: context,
                           destination: "/Qr_code_Generate_Data",
                           buttonTitle: 'QR code Rapport',
-                          buttonText: 'Ce QR code permet de accéder et envoyer le rapport de toutes les traitement UVC sur votre téléphone',
-                          buttonDescription1: 'Créer un QR code contenant :',
-                          buttonDescription2: '- une adresse Email'),
-                      qrCodeGenerator(
+                          buttonText:
+                              'Ce QR code permet d\'afficher et d\'envoyer le rapport des désinfections par mail à un adresse préenregistrée.'),
+                      qrCodeGeneratorSecond(
                           context: context,
                           destination: "/Qr_code_Display_Security",
                           buttonTitle: 'QR code Sécurité',
-                          buttonText: 'Ce QR code de sécurité pour les robots UV-C',
-                          buttonDescription1: 'Créer un QR code contenant :',
-                          buttonDescription2: '- un lien de securité sur le site de deeplight.com'),
+                          buttonText: 'Ce QR code permet de créer un QR code de sécurité.'),
                     ],
                   ),
                 ),
@@ -138,6 +135,55 @@ class _ChooseQrCodeState extends State<ChooseQrCode> {
                 child: Text(
                   buttonDescription2,
                   textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: screenWidth * 0.05,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector qrCodeGeneratorSecond({BuildContext context, String destination, String buttonTitle, String buttonText}) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushReplacementNamed(context, destination);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+          shape: RoundedRectangleBorder(side: new BorderSide(color: Colors.blue[400], width: 2.0), borderRadius: BorderRadius.circular(4.0)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                child: FlatButton(
+                  onPressed: () => Navigator.pushReplacementNamed(context, destination),
+                  child: Text(
+                    buttonTitle,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.05,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  color: Colors.blue[400],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  buttonText,
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: screenWidth * 0.05,
