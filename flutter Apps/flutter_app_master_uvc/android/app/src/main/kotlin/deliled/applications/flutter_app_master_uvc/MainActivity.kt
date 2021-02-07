3bd29ff0e451171ee275dc682590e7456d858c73
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2021-02-07 12:49:11 +0100

    update master uvc

--- flutter Apps/flutter_app_master_uvc/android/app/src/main/kotlin/deliled/applications/flutter_app_master_uvc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package deliled.applications.flutter_app_master_uvc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ flutter Apps/flutter_master_uvc_app/android/app/src/main/kotlin/deliled/applications/flutter_master_uvc_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package deliled.applications.flutter_master_uvc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
