4f43ad07ccc56837c0c28685b35f54a70a29c9d3
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2021-12-09 17:06:57 +0100

    Update languageDataBase.dart

--- flutter Apps/flutter_app_master_uvc/lib/services/languageDataBase.dart
+++ flutter Apps/flutter_app_master_uvc/lib/services/languageDataBase.dart
@@ -10,30 +10,30 @@ final List<String> okTextLanguageArray = ['OK', 'OK'];
 
 final List<String> cancelTextLanguageArray = ['Annuler', 'Cancel'];
 
-final List<String> startScanAlertDialogTextLanguageArray = ['Scanner le QR code du dispositif UV-C DEEPLIGHT®.', 'Scan the QR code of the UV-C DEEPLIGHT® device.'];
+final List<String> startScanAlertDialogTextLanguageArray = ['Scanner le QR code du dispositif UV-C DEEPLIGHT®.', 'Scan the the UV-C DEEPLIGHT® device QR code.'];
 
 final List<String> checkBluetoothMessageTextLanguageArray = [
   'Afin de garantir le bon fonctionnement de l\'application merci d\'activer votre Bluetooth.',
-  'To ensure the proper functioning of the application, please activate your Bluetooth.'
+  'To ensure the proper fonctionnement of the application, please activate your Bluetooth.'
 ];
 
 final List<String> checkLocationMessageTextLanguageArray = [
-  'Afin de garantir le bon fonctionnement de l\'application merci d\'activer votre Location.',
-  'To ensure the proper functioning of the application, please activate your Location.'
+  'Afin de garantir le bon fonctionnement de l\'application merci d\'activer votre localisation.',
+  'Please activate your location to ensure the proper running of the application.'
 ];
 
 final List<String> checkPermissionsTitleTextLanguageArray = ['Permissions', 'Permissions'];
 
-final List<String> bluetoothToastLanguageArray = ['Le Bluetooth (BLE) n\'est pas activé sur votre téléphone !', 'Bluetooth (BLE) is not activated on your phone !'];
+final List<String> bluetoothToastLanguageArray = ['Le Bluetooth n\'est pas activé sur votre téléphone !', 'Bluetooth is not activated on your phone!'];
 
 final List<String> checkPermissionMessageTextLanguageArray = [
   'Afin de garantir le bon fonctionnement de l\'application merci d\'activer votre Bluetooth ainsi que votre localisation.',
-  'To ensure the proper functioning of the application, please activate your Bluetooth as well as your location.'
+  'Please activate your Bluetooth and location to ensure the proper running of the application.'
 ];
 
-final List<String> understoodTextLanguageArray = ['Compris', 'Understood'];
+final List<String> understoodTextLanguageArray = ['Compris', 'I understand'];
 
-final List<String> cameraStartToastTextLanguageArray = ['Lancement de la caméra !', 'Camera launch!'];
+final List<String> cameraStartToastTextLanguageArray = ['Lancement de la caméra !', 'Camera launching!'];
 
 final List<String> qrCodeValidMessageTextLanguageArray = ['Accès valide', 'Valid access'];
 
@@ -41,48 +41,48 @@ final List<String> qrCodeNonValidMessageTextLanguageArray = ['Accès non valide'
 
 final List<String> cameraPageTitleTextLanguageArray = ['Scanner le QR code', 'Scan the QR code'];
 
-final List<String> cameraPageTorchButtonTextLanguageArray = ['Lampe torche', 'Torch'];
+final List<String> cameraPageTorchButtonTextLanguageArray = ['Lampe torche', 'Light'];
 
-final List<String> checkConnectionAlertDialogTitleTextLanguageArray = ['Connexion en cours', 'Checking connection'];
+final List<String> checkConnectionAlertDialogTitleTextLanguageArray = ['Connexion en cours', 'In progress connection'];
 
 final List<String> checkConnectionAlertDialogMessageTextLanguageArray = [
   'Veuillez rester près du dispositif UV-C pendant quelques instants, la connexion est en cours.',
-  'Please stay near the UV-C device for a while, the connection is in progress.'
+  'Please stay near the UV-C device for a while, connection in progress.'
 ];
 
 final List<String> authoriseConnectionToastTextLanguageArray = ['Autorisation de connexion validée !', 'Connection authorization validated!'];
 
-final List<String> startScanAlertDialogTitleTextLanguageArray = ['Scanner le QR code du dispositif UV-C DEEPLIGHT.', 'Scan the QR code of the UV-C DEEPLIGHT device.'];
+final List<String> startScanAlertDialogTitleTextLanguageArray = ['Scanner le QR code du dispositif UV-C DEEPLIGHT.', 'Scan the UV-C DEEPLIGHT device QR code.'];
 
-final List<String> scanBLEListTitleTextLanguageArray = ['Liste des UVC-LIGHT :', 'List of UVC-LIGHT :'];
+final List<String> scanBLEListTitleTextLanguageArray = ['Liste des solutions UV-C :', 'UV-C devices list:'];
 
 final List<String> warningMessageTextLanguageArray = [
-  'Cette application ne fonctionne pas avec votre dispositif UV-C, veuillez contacter DeliTech Medical® pour plus d\'informations en cliquant ',
-  'This app does not work with your UV-C device, please contact DeliTech Medical® for more information by clicking '
+  'Cette application n\'est pas comptaible avec votre dispositif UV-C, veuillez nous contacter en cliquant ',
+  'This app does not work with your UV-C device, please contact us by clicking '
 ];
 
 final List<String> warningLinkTextLanguageArray = ['ici', 'here'];
 
-final List<String> exitAlertDialogMessageTextLanguageArray = ['Voulez-vous quitter cette page ?', 'Do you want to leave this page ?'];
+final List<String> exitAlertDialogMessageTextLanguageArray = ['Voulez-vous quitter cette page ?', 'Do you want to leave this page?'];
 
-final List<String> checkConnectionToastMessageTextLanguageArray = ['Le dispositif est trop loin ou éteint, merci de vérifier ce dernier', 'The device is too far away or turned off, please check it'];
+final List<String> checkConnectionToastMessageTextLanguageArray = ['Dispositif trop loin ou éteint, merci de le vérifier', 'Device too far or turned off, please inspect it'];
 
 final List<String> activatedButtonTextLanguageArray = ['Activée', 'Activated'];
 
 final List<String> deactivatedButtonTextLanguageArray = ['Désactivée', 'Deactivated'];
 
-final List<String> configSavedToastTextLanguageArray = ['Configuration sauvegardée !', 'Configuration saved !'];
+final List<String> configSavedToastTextLanguageArray = ['Configuration sauvegardée !', 'Setting saved!'];
 
 final List<String> activatedTextLanguageArray = ['activé', 'activated'];
 
 final List<String> deactivatedTextLanguageArray = ['désactivé', 'deactivated'];
 
-final List<String> scanQrCodeTitleAlertDialogTextLanguageArray = ['Scan QrCode Securité :', 'Scan Qr Code Security :'];
+final List<String> scanQrCodeTitleAlertDialogTextLanguageArray = ['Scan du QR code de sécurité :', 'Security QR code scan:'];
 
 final List<String> deactivationScanQrCodeMessageAlertDialogTextLanguageArray = [
   'Attention : désactiver le QR code de sécurité peut entraîner des risques et engage votre responsabilité. '
       'Il vous appartient de notifier les utilisateurs finaux des solutions UV-C DEEPLIGHT® de la désactivation de ce paramètre. ',
-  'Warning: deactivating the security QR code can entail risks and engages your responsibility. '
+  'Warning: deactivating the security QR code can cause risks and engages your responsibility. '
       'It is your responsibility to notify the end users of UV-C DEEPLIGHT® solutions of the deactivation of this parameter. '
 ];
 
@@ -94,7 +94,7 @@ final List<String> manualTextLanguageArray = ['manuel', 'manual'];
 
 final List<String> automaticTextLanguageArray = ['automatique', 'automatic'];
 
-final List<String> pilotAlertDialogTitleTextLanguageArray = ['Type de pilotage:', 'Type of piloting:'];
+final List<String> pilotAlertDialogTitleTextLanguageArray = ['Type de pilotage:', 'Controling type:'];
 
 final List<String> informationAlertDialogTitleTextLanguageArray = ['Information :', 'Information:'];
 
@@ -102,7 +102,7 @@ final List<String> pilotModeAlertDialogMessageTextLanguageArray = [
   'En cliquant sur \"Changer\", le dispositif UV-C passera soit en mode Manuel soit en mode Automatique. \n\n'
       'Manuel : configurez manuellement les données de désinfection (entreprise, opérateur, durée de désinfection). \n\n'
       'Automatique : permet de lire des QR codes contenant des données de désinfection pré-configurées.',
-  'By clicking on \ "Change \", the UV-C device will switch to either Manual mode or Automatic mode. \n\n'
+  'By clicking \ "Change \", the UV-C device will switch either to Manual mode or Automatic mode. \n\n'
       'Manual: manually configure the disinfection data (company, operator, disinfection duration). \n\n'
       'Automatic: allows you to read QR codes containing pre-configured disinfection data.'
 ];
@@ -111,7 +111,7 @@ final List<String> securityQrCodeAlertDialogMessageTextLanguageArray = [
   'En cliquant sur \"Changer\", le dispositif UV-C passera soit en mode Activé ou Désactivé. \n\n'
       'Activé : le QR code de sécurité vous sera demandé à chaque fois. \n\n'
       'Désactivé : le QR code de sécurité ne vous sera plus jamais demandé.',
-  'By clicking on \ "Change \", the UV-C device will switch to either On or Off mode. \n\n'
+  'By clicking \ "Change \", the UV-C device will switch to either Activated or Disable mode. \n\n'
       'Activated: you will be asked for the security QR code each time. \n\n'
       'Disabled: You will never be asked for the security QR code again.'
 ];
@@ -120,13 +120,13 @@ final List<String> secondsTextLanguageArray = ['secondes', 'seconds'];
 
 final List<String> minutesTextLanguageArray = ['minutes', 'minutes'];
 
-final List<String> hoursTextLanguageArray = ['heures', 'Hours'];
+final List<String> hoursTextLanguageArray = ['heures', 'hours'];
 
 final List<String> timesTextLanguageArray = ['fois', 'times'];
 
 final List<String> masterUVCTitleTextLanguageArray = ['Master UVC', 'Master UVC'];
 
-final List<String> resetAlertDialogMessageTextLanguageArray = ['Êtes-vous sûr de vouloir remettre à zéro ces données ?', 'Are you sure you want to reset this data to zero ?'];
+final List<String> resetAlertDialogMessageTextLanguageArray = ['Êtes-vous sûr de vouloir remettre à zéro ces données ?', 'Are you sure you want to reset these data?'];
 
 final List<String> resetButtonTextLanguageArray = ['Remise à zero', 'Reset'];
 
@@ -138,8 +138,8 @@ final List<String> maintenanceUVCTextLanguageArray = ['Maintenance des tubes UV-
 
 final List<String> changeTextLanguageArray = ['changer', 'change'];
 
-final List<String> qrCodeSecurityTextLanguageArray = ['QR code de sécurité :', 'QR security code:'];
+final List<String> qrCodeSecurityTextLanguageArray = ['QR code de sécurité :', 'Security QR code:'];
 
-final List<String> pilotTypeTextLanguageArray = ['Type de pilotage :', 'Type of piloting :'];
+final List<String> pilotTypeTextLanguageArray = ['Type de pilotage :', 'Controling type:'];
 
-final List<String> parametersUVCTextLanguageArray = ['Paramètres du dispositif UV-C', 'UV-C Device Parameters'];
+final List<String> parametersUVCTextLanguageArray = ['Paramètres du dispositif UV-C', 'UV-C device settings'];
