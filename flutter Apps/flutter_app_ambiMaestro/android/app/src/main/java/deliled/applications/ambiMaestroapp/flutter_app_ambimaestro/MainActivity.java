2ecfc3b4a3eff6986a022da87af7b75e82cded9b
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2022-01-14 14:25:27 +0100

    add new app ambiMaestro

---
+++ flutter Apps/flutter_app_ambiMaestro/android/app/src/main/java/deliled/applications/ambiMaestroapp/flutter_app_ambimaestro/MainActivity.java
@@ -0,0 +1,6 @@
+package deliled.applications.ambiMaestroapp.flutter_app_ambimaestro;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter Apps/flutter_app_ambiMaestro/lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app_ambimaestro/pages/check_permissions.dart';
+import 'package:flutter_app_ambimaestro/pages/scan_ble_list.dart';
+import 'package:flutter_app_ambimaestro/pages/welcome.dart';
+import 'package:flutter_app_ambimaestro/services/data_variables.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:wakelock/wakelock.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+// Streams are created so that app can respond to notification-related events since the plugin is initialised in the `main` function
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject = BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String> selectNotificationSubject = BehaviorSubject<String>();
+
+NotificationAppLaunchDetails? notificationAppLaunchDetails;
+
+class ReceivedNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+}
+
+void main() async {
+  // needed if you intend to initialize in the `main` function
+  WidgetsFlutterBinding.ensureInitialized();
+
+  notificationAppLaunchDetails = await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  var initializationSettingsAndroid = const AndroidInitializationSettings('@mipmap/ic_launcher');
+  // Note: permissions aren't requested here just to demonstrate that can be done later using the `requestPermissions()` method
+  // of the `IOSFlutterLocalNotificationsPlugin` class
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification: (int? id, String? title, String? body, String? payload) async {
+        didReceiveLocalNotificationSubject.add(ReceivedNotification(id: id!, title: title!, body: body!, payload: payload!));
+      });
+  var initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: (String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    selectNotificationSubject.add(payload!);
+  });
+  flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+
+  SystemChannels.textInput.invokeMethod('TextInput.hide');
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
+  Wakelock.enable();
+  bool wakelockEnabled = await Wakelock.enabled;
+  if (wakelockEnabled) {
+    // The following statement disables the wakelock.
+    Wakelock.toggle(enable: true);
+  }
+
+  languageCode = Platform.localeName.split('_')[0];
+
+  if (languageCode.isEmpty) {
+    languageCode = 'en';
+  }
+
+  switch (languageCode) {
+    case 'fr':
+      languageArrayIdentifier = 0;
+      break;
+    case 'en':
+      languageArrayIdentifier = 1;
+      break;
+    default:
+      languageArrayIdentifier = 1;
+      break;
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Lumi\'air Lite',
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const Welcome(),
+        '/check_permissions': (context) => const CheckPermissions(),
+        '/scan_ble_list': (context) => const ScanListBle(),
+      },
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
