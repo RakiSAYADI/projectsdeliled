6e2b576dd3eb0f51ef5402051fafca49f9be9cdf
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2022-04-19 14:59:23 +0200

    update lumyhome app

--- flutter Apps/flutter_app_dmx_maestro/lib/services/DataVariables.dart
+++ flutter Apps/flutter_app_dmx_maestro/lib/services/DataVariables.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_dmx_maestro/services/bleDeviceClass.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
-final String appName = 'Lumi\'Home';
+final String appName = 'LumyHome';
 
 final double hueCoefficient = (256 / 360);
 
