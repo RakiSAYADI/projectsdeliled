0a62a818fb1614696e06c3e0192726d927698dd5
Raki SAYADI <70013775+RakiSAYADI@users.noreply.github.com>
2022-12-05 17:22:23 +0100

    update delismartconfig app

--- flutter Apps/flutter_delismart_desktop_app/lib/pages/welcome.dart
+++ flutter Apps/flutter_delismart_desktop_app/lib/pages/welcome.dart
@@ -60,7 +60,7 @@ class _WelcomeState extends State<Welcome> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/fond-lumiair-lite.tif'),
+            image: AssetImage('assets/fond-lumiair-lite.jpg'),
             fit: BoxFit.cover,
           ),
         ),
